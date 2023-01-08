a463607742687d5998e0a3b84cb3e5115ec1b791
puja2903 <110466374+puja2903@users.noreply.github.com>
2023-01-08 20:12:24 +0530

    Add files via upload

---
+++ D_tech management system/ManagementNotFoundException.java
@@ -0,0 +1,12 @@
+package com.D_tech.exception;
+
+public class ManagementNotFoundException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+
+	public ManagementNotFoundException(String msg) {
+		super(msg);
+		
+	}
+
+}
