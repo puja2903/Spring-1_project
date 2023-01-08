1994d94db53f299a536b8138e4a0b453dab6caa7
puja2903 <110466374+puja2903@users.noreply.github.com>
2023-01-08 20:15:44 +0530

    Add files via upload

---
+++ D_tech management system/D_techApplication.java
@@ -0,0 +1,13 @@
+package com.D_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D_techApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D_techApplication.class, args);
+	}
+
+}
