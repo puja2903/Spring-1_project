c331e2f5343eb341b79ffc9bb22d5abbe477289c
puja2903 <110466374+puja2903@users.noreply.github.com>
2023-01-08 20:11:49 +0530

    Add files via upload

---
+++ D_tech management system/ErrorMessage.java
@@ -0,0 +1,28 @@
+package com.D_tech.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorMessage {
+	
+	private HttpStatus status;
+	private String message;
+	
+	public ErrorMessage(HttpStatus status, String message) {
+		super();
+		this.status = status;
+		this.message = message;
+	}
+	public HttpStatus getStatus() {
+		return status;
+	}
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
